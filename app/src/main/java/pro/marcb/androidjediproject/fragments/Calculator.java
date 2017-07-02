d56a7a1dcfd8ebbbeb10e5320cc8eb527ee98c87
Marc Benedí <marcbenedi.sanmillan@gmail.com>
2017-07-02 13:33:26 +0200

    Calculator done. Some crashes in inputs like 3+-4 And pressing Del with empty expressions

--- app/src/main/java/pro/marcb/androidjediproject/fragments/Calculator.java
+++ app/src/main/java/pro/marcb/androidjediproject/fragments/Calculator.java
@@ -3,11 +3,15 @@ package pro.marcb.androidjediproject.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.HorizontalScrollView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import pro.marcb.androidjediproject.R;
@@ -16,25 +20,19 @@ public class Calculator extends Fragment{
 
     private final static String TAG = "Calculator";
 
-    private int[] numbers = {R.id.button0, R.id.button1, R.id.button2, R.id.button3, R.id.button4,
-                         R.id.button5, R.id.button6, R.id.button7, R.id.button8, R.id.button9
-                        };
-
     private Button button0, button1, button2, button3, button4, button5, button6, button7, button8, button9;
     private Button buttonDot, buttonDel, buttonDiv, buttonMul, buttonSub, buttonSum, buttonEq;
 
-    private TextView textActual, textAcumulat;
+    private TextView textTempResult, textAllExpression;
     private HorizontalScrollView scrollView;
 
-    private boolean opPressed = false;
-    private double number = 0;
-    private boolean dot = false;
+    private String expression = "";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View v = inflater.inflate(R.layout.fragment_calculator, container, false);
+        final View v = inflater.inflate(R.layout.fragment_calculator, container, false);
 
         button0 = (Button) v.findViewById(R.id.button0);
         button0.setOnClickListener(numberClick);
@@ -71,8 +69,25 @@ public class Calculator extends Fragment{
         buttonDel = (Button) v.findViewById(R.id.buttonDel);
         buttonDel.setOnClickListener(opClick);
 
-        textActual = (TextView) v.findViewById(R.id.textActual);
-        textAcumulat = (TextView) v.findViewById(R.id.textAcumulat);
+        buttonDel.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+
+                LinearLayout l = (LinearLayout) v.findViewById(R.id.results);
+                TypedValue outValue = new TypedValue();
+                getContext().getTheme().resolveAttribute(R.drawable.ripple_effect_clear, outValue, true);
+                l.setBackgroundResource(outValue.resourceId);
+
+                expression = "";
+                textTempResult.setText("");
+                textAllExpression.setText("");
+
+                return false;
+            }
+        });
+
+        textTempResult = (TextView) v.findViewById(R.id.textActual);
+        textAllExpression = (TextView) v.findViewById(R.id.textAcumulat);
 
         scrollView = (HorizontalScrollView) v.findViewById(R.id.scrollView);
 
@@ -87,10 +102,19 @@ public class Calculator extends Fragment{
 
     }
 
-    private void updateTextView(char c){
-        textActual.setText(textActual.getText().toString()+c);
-        textAcumulat.setText(textAcumulat.getText().toString()+c);
+    private void numberPressed(char c){
+        expression += String.valueOf(c);
+
+        textAllExpression.setText(expression);
         scrollView.fullScroll(View.FOCUS_RIGHT);
+
+        textTempResult.setText(String.valueOf(new Tree(expression).eval()));
+
+    }
+
+    private void opPressed(char op){
+        expression += String.valueOf(op);
+        textAllExpression.setText(expression);
     }
 
     private View.OnClickListener numberClick = new View.OnClickListener() {
@@ -98,34 +122,34 @@ public class Calculator extends Fragment{
         public void onClick(View view) {
             switch (view.getId()){
                 case R.id.button0:
-                    updateTextView('0');
+                    numberPressed('0');
                     break;
                 case R.id.button1:
-                    updateTextView('1');
+                    numberPressed('1');
                     break;
                 case R.id.button2:
-                    updateTextView('2');
+                    numberPressed('2');
                     break;
                 case R.id.button3:
-                    updateTextView('3');
+                    numberPressed('3');
                     break;
                 case R.id.button4:
-                    updateTextView('4');
+                    numberPressed('4');
                     break;
                 case R.id.button5:
-                    updateTextView('5');
+                    numberPressed('5');
                     break;
                 case R.id.button6:
-                    updateTextView('6');
+                    numberPressed('6');
                     break;
                 case R.id.button7:
-                    updateTextView('7');
+                    numberPressed('7');
                     break;
                 case R.id.button8:
-                    updateTextView('8');
+                    numberPressed('8');
                     break;
                 case R.id.button9:
-                    updateTextView('9');
+                    numberPressed('9');
                     break;
             }
         }
@@ -136,34 +160,169 @@ public class Calculator extends Fragment{
         public void onClick(View view) {
             switch (view.getId()){
                 case R.id.buttonDiv:
-                    updateTextView('/');
+                    opPressed('/');
                     break;
                 case R.id.buttonMul:
-                    updateTextView('x');
+                    opPressed('x');
                     break;
                 case R.id.buttonSub:
-                    updateTextView('-');
+                    opPressed('-');
                     break;
                 case R.id.buttonPlus:
-                    updateTextView('+');
+                    opPressed('+');
                     break;
                 case R.id.buttonEq:
-                    updateTextView('=');
+                    textAllExpression.setText(String.valueOf(new Tree(expression).eval()));
+                    expression = "";
+                    textTempResult.setText("");
+
+                    Animation bottomToTop = AnimationUtils.loadAnimation(getContext(), R.anim.result_calculator);
+                    textAllExpression.startAnimation(bottomToTop);
+
+                    //TODO:
                     break;
                 case R.id.buttonDot:
-                    updateTextView('.');
+                    expression +='.';
                     break;
                 case R.id.buttonDel:
-                    updateTextView('D');
+                    //TODO: onLongClickListener
+                    expression = expression.substring(0, expression.length()-1);
+                    textAllExpression.setText(expression);
+                    char last = expression.charAt(expression.length()-1);
+                    if (last != '+' && last != '-' && last != 'x' && last != '/'){
+                        textAllExpression.setText(expression);
+                        scrollView.fullScroll(View.FOCUS_RIGHT);
+
+                        textTempResult.setText(String.valueOf(new Tree(expression).eval()));
+                    }
                     break;
             }
         }
     };
 
-    private boolean isNumber(int id){
-        for (int i:numbers) {
-            if (i == id)return true;
+    private class Tree {
+        //TODO: Problem with op followed by op i.e: 2x-4
+        private Tree left;
+        private Tree right;
+        private boolean isNumber;
+        private double number;
+        private char op;
+
+        public Tree(String s) {
+
+            int index;
+            int index2;
+            int lastFound;
+            String left;
+            String right;
+
+            //Base case: A number
+            if (isNumeric(s)) {
+                left = null;
+                right = null;
+                isNumber = true;
+                number = Double.valueOf(s);
+            } else {
+                //We search the last + or /
+                index = searchFor('+', s);
+                index2 = searchFor('-', s);
+
+                lastFound = index < index2 ? index2 : index;
+
+                if (lastFound != -1) {
+
+                    left = s.substring(0, lastFound);
+                    right = s.substring(lastFound + 1, s.length());
+
+                    this.left = left.length()==0? null : new Tree(left);
+                    this.right = right.length()==0? null : new Tree(right);
+
+                    isNumber = false;
+                    op = s.charAt(lastFound);
+
+                } else {
+                    //Search for x or /
+
+                    index = searchFor('x', s);
+                    index2 = searchFor('/', s);
+
+                    lastFound = index < index2 ? index2 : index;
+
+                    if (lastFound != -1) {
+
+                        left = s.substring(0, lastFound );
+                        right = s.substring(lastFound + 1, s.length());
+
+                        this.left = left.length()==0? null : new Tree(left);
+                        this.right = right.length()==0? null : new Tree(right);
+
+                        isNumber = false;
+                        op = s.charAt(lastFound);
+
+                    }
+
+                }
+
+            }
+
         }
-        return false;
+
+        private boolean isNumeric(String str)
+        {
+            return str.matches("[+-]?\\d+(\\.\\d+)?");  //match a number with optional '-' and decimal.
+        }
+
+        private int searchFor(char c, String s){
+            int size = s.length();
+            int index = -1;
+            boolean found = false;
+            for (int i = size-1; i >= 0 && !found; --i){
+                char el = s.charAt(i);
+                if (c == el) {
+                    found = true;
+                    index = i;
+                }
+            }
+            return index;
+        }
+
+        public void print(){
+            String print = isNumber? String.valueOf(number) : String.valueOf(op);
+            System.out.println(print);
+            if (left != null) left.print();
+            if (right != null) right.print();
+
+        }
+
+        public double eval() {
+
+            if (isNumber) return number;
+            else {
+                double left = this.left.eval();
+                double right = this.right.eval();
+                double result;
+                switch (op){
+                    case '+':
+                        result = left + right;
+                        break;
+                    case '-':
+                        result = left - right;
+                        break;
+                    case 'x':
+                        result = left * right;
+                        break;
+                    case '/':
+                        result = left / right;
+                        break;
+                    default:
+                        return 0;
+                }
+
+                return result;
+            }
+
+        }
+
     }
+
 }
